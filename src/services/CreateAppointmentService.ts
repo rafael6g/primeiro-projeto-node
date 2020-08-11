8f612112f454799ee0a77ca04bf764153dd46eee
Rafael C Fernandes <rafael.londrina@gmail.com>
2020-08-10 22:02:00 -0300

    primeiro projeto nodeJS

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,39 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  // isto e um DTO
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentinSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentinSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
